4eb95cf56d1f32d131ed3a32ae50a079b03d395f
Eduardo Slomp Arán <du_aran@hotmail.com>
2025-04-10 20:58:25 -0300

    feature: busca em lagura BFS implementada

--- main.py
+++ main.py
@@ -1,104 +1,64 @@
-from src.functions import Grafos, GrafoMatriz, GrafoLista
+from src.grafos import Grafos, GrafoMatriz, GrafoLista
  
-print("______________________________________________")
-print("\nINICIANDO TESTES DA CLASSE GRAFOLISTA\n")
-print("----------\n")
-    
-# Criando um grafo não-direcionado e ponderado
-grafoL = GrafoLista(direcionado=False, ponderado=True)
-    
-# Inserindo vértices
-grafoL.inserirVertice("A")
-grafoL.inserirVertice("B")
-grafoL.inserirVertice("C")
-grafoL.inserirVertice("D")
-print("Vértices inseridos com sucesso!\n")
-    
-# Inserindo arestas
-grafoL.inserirAresta("A", "B", 5)  # A - B
-grafoL.inserirAresta("B", "C", 3)  # B - C
-grafoL.inserirAresta("C", "D", 2)  # C - D
-grafoL.inserirAresta("D", "A", 4)  # D - A
-print("Arestas inseridas com sucesso!\n")
+def main():
 
-# Imprimindo representação do grafo
-grafoL.imprimeGrafo()
-print("")
+    print("""
+░█▀▀█ ░█▀▀█ ▀█▀ ░█▀▄▀█ ░█▀▀▀ ▀█▀ ░█▀▀█ ░█▀▀▀█ 　 ░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀▀ ░█▀▀▀█ 
+░█▄▄█ ░█▄▄▀ ░█─ ░█░█░█ ░█▀▀▀ ░█─ ░█▄▄▀ ░█──░█ 　 ░█─▄▄ ░█▄▄▀ ░█▄▄█ ░█▀▀▀ ░█──░█ 
+░█─── ░█─░█ ▄█▄ ░█──░█ ░█▄▄▄ ▄█▄ ░█─░█ ░█▄▄▄█ 　 ░█▄▄█ ░█─░█ ░█─░█ ░█─── ░█▄▄▄█""")
     
-# Verificando existência de arestas
-print(f"Existe aresta entre A e B? {grafoL.existeAresta(0, 1)}")
-print(f"Existe aresta entre B e D? {grafoL.existeAresta(1, 3)}")
-print("")
+    print("______________________________\n")
+    grafo_teste_01 = GrafoLista(direcionado=False, ponderado=False)
+    Grafos.carregar_grafo_arquivo(grafo_teste_01, "data/grafos/grafo_teste_01.txt")
+    print("______________________________")
+
+    print("\nBuscas:\n")
+    grafo_teste_01.busca_em_largura("0")
+    grafo_teste_01.busca_em_largura("1")
+    grafo_teste_01.busca_em_largura("2")
+
+    print("\n\n")
+
+    print("""
+
+░█▀▀▀█ ░█▀▀▀ ░█▀▀█ ░█─░█ ░█▄─░█ ░█▀▀▄ ░█▀▀▀█ 　 ░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀▀ ░█▀▀▀█ 
+─▀▀▀▄▄ ░█▀▀▀ ░█─▄▄ ░█─░█ ░█░█░█ ░█─░█ ░█──░█ 　 ░█─▄▄ ░█▄▄▀ ░█▄▄█ ░█▀▀▀ ░█──░█ 
+░█▄▄▄█ ░█▄▄▄ ░█▄▄█ ─▀▄▄▀ ░█──▀█ ░█▄▄▀ ░█▄▄▄█ 　 ░█▄▄█ ░█─░█ ░█─░█ ░█─── ░█▄▄▄█""")
     
-# Verificando pesos das arestas
-print(f"Peso da aresta A - B: {grafoL.pesoAresta(0, 1)}")
-print(f"Peso da aresta C - D: {grafoL.pesoAresta(2, 3)}")
-print("\n")
-    
-# Retornando vizinhos
-print(f"Vizinhos de A: {grafoL.retornarVizinhos(0)}")
-print(f"Vizinhos de B: {grafoL.retornarVizinhos(1)}")
-print("")
-    
-# Removendo uma aresta
-grafoL.removerAresta(0)
-print("Aresta entre A e B removida!\n")
-    
-# Imprimindo representação do grafo após remoção
-grafoL.imprimeGrafo()
+    print("______________________________\n")
+    grafo_teste_02 = GrafoLista(direcionado=True, ponderado=True)
+    Grafos.carregar_grafo_arquivo(grafo_teste_02, "data/grafos/grafo_teste_02.txt")
+    print("______________________________")
+
+    print("\nBuscas:\n")
+    grafo_teste_02.busca_em_largura("0")
+    grafo_teste_02.busca_em_largura("1")
+    grafo_teste_02.busca_em_largura("2")
+    grafo_teste_02.busca_em_largura("3")
+
+    print("\n\n")
+
+    print("""
 
 
-print("______________________________________________")
-print("\nINICIANDO TESTES DA CLASSE GRAFOMATRIZ\n")
-print("----------\n")
+▀▀█▀▀ ░█▀▀▀ ░█▀▀█ ░█▀▀█ ░█▀▀▀ ▀█▀ ░█▀▀█ ░█▀▀▀█ 　 ░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀▀ ░█▀▀▀█ 
+─░█── ░█▀▀▀ ░█▄▄▀ ░█─── ░█▀▀▀ ░█─ ░█▄▄▀ ░█──░█ 　 ░█─▄▄ ░█▄▄▀ ░█▄▄█ ░█▀▀▀ ░█──░█ 
+─░█── ░█▄▄▄ ░█─░█ ░█▄▄█ ░█▄▄▄ ▄█▄ ░█─░█ ░█▄▄▄█ 　 ░█▄▄█ ░█─░█ ░█─░█ ░█─── ░█▄▄▄█""")
     
-# Criando um grafo não-direcionado e ponderado
-grafoM = GrafoMatriz(direcionado=False, ponderado=True)
-    
-# Inserindo vértices
-grafoM.inserirVertice("A")
-grafoM.inserirVertice("B")
-grafoM.inserirVertice("C")
-grafoM.inserirVertice("D")
-print("Vértices inseridos com sucesso!\n")
-    
-# Inserindo arestas
-grafoM.inserirAresta(0, 1, 5)  # A - B
-grafoM.inserirAresta(1, 2, 3)  # B - C
-grafoM.inserirAresta(2, 3, 2)  # C - D
-grafoM.inserirAresta(3, 0, 4)  # D - A
-print("Arestas inseridas com sucesso!\n")
+    print("______________________________\n")
+    grafo_teste_03 = GrafoMatriz(direcionado=False, ponderado=False)
+    Grafos.carregar_grafo_arquivo(grafo_teste_03, "data/grafos/grafo_teste_03.txt")
+    print("______________________________")
 
-# Imprimindo matriz de adjacência
-print("\nMatriz de Adjacência:\n")
-grafoM.imprimeGrafo()
-print("")
-    
-# Verificando existência de arestas
-print(f"Existe aresta entre A e B? {grafoM.existeAresta(0, 1)}")
-print(f"Existe aresta entre B e D? {grafoM.existeAresta(1, 3)}")
-print("")
-    
-# Verificando pesos das arestas
-print(f"Peso da aresta A - B: {grafoM.pesoAresta(0, 1)}")
-print(f"Peso da aresta C - D: {grafoM.pesoAresta(2, 3)}")
-print("\n")
-    
-# Retornando vizinhos
-print(f"Vizinhos de A: {grafoM.retornarVizinhos(0)}")
-print(f"Vizinhos de B: {grafoM.retornarVizinhos(1)}")
-print("")
-    
-# Removendo uma aresta
-grafoM.removerAresta(0, 1)
-print("Aresta entre A e B removida!\n")
-    
-# Imprimindo matriz após remoção
-print("Matriz de Adjacência após remoção da aresta A-B:\n")
-grafoM.imprimeGrafo()
-print("")
-    
-# Imprimindo representação do grafo
-grafoM.imprimeGrafo()
+    grafo_teste_03.imprimeGrafo()
+    print("\nBuscas:\n")
+    grafo_teste_03.busca_em_largura("0")
+    grafo_teste_03.busca_em_largura("1")
+    grafo_teste_03.busca_em_largura("2")
+    grafo_teste_03.busca_em_largura("3")
+    grafo_teste_03.busca_em_largura("4")
 
-
+    print("\n\n")
+    
+if __name__ == "__main__":
+    main()
